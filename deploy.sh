4b4627cbad31ab66ef17521b40cd2e769606ba99
antonvh <antonhaninvl@gmail.com>
2021-07-16 16:57:09 +0300

    add config for host

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.avhhost.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+cd -
